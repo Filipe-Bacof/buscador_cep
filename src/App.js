fd17b8631809a946bfc4767a1589c71ecd06fb0d
Filipe Bacof <filipebacof@gmail.com>
2022-09-14 17:34:08 -0300

    Style aplicado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>TESTE PROJETO</h1>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite o seu CEP..."
+        />
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 93222-222</h2>
+        <span>Rua Teste Algum</span>
+        <span>Complemento</span>
+        <span>Vila Rosa</span>
+        <span>Sapucaia do Sul - RS</span>
+      </main>
     </div>
   );
 }
