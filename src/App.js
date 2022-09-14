3749f5edc2dd746f1729c3ad4e0e6b4dbddea45f
Filipe Bacof <filipebacof@gmail.com>
2022-09-14 18:05:09 -0300

    implementada api - console.log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
 import './styles.css';
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  async function handleSearch () {
+    // 93230495/json/
+
+    if (input === '') {
+      alert("Preencha algum CEP");
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response);
+    }catch{
+      alert("ERRO AO BUSCAR");
+    }
+
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
@@ -9,17 +31,19 @@ function App() {
         <input
         type="text"
         placeholder="Digite o seu CEP..."
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
         />
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
 
       <main className='main'>
         <h2>CEP: 93222-222</h2>
-        <span>Rua Teste Algum</span>
-        <span>Complemento</span>
-        <span>Vila Rosa</span>
+        <span>Rua da sua mãe</span>
+        <span>Complemento prostibulo</span>
+        <span>Ipiranga</span>
         <span>Sapucaia do Sul - RS</span>
       </main>
     </div>
